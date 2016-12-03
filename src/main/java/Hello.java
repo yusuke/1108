6a4b88bab0a5765bbb7350edb791b9d2c204e488
Yusuke Yamamoto <yusuke@mac.com>
2016-12-03 17:58:12 +0900

    hoge

---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * Created by yusuke on 2016/12/03.
+ */
+public class Hello {
+    public void helloWorld(){
+        System.out.println("hello world");
+    }
+}
---
+++ src/main/java/World.java
@@ -0,0 +1,9 @@
+/**
+ * Created by yusuke on 2016/12/03.
+ */
+public class World {
+    public void helloWorld(){
+        System.out.println("hello world");
+    }
+
+}
